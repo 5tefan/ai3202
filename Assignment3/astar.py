7759f7b9e024cce5cfbda2d18672f72e632d182a
Stefan Codrescu <ssmmcc1@gmail.com>
2015-09-16 23:57:54 -0600

    final adjustments, printing number of locations evaluated, added [None, None] return in case there is no valid path

--- Assignment3/astar.py
+++ Assignment3/astar.py
@@ -1,3 +1,10 @@
+#!/bin/python
+
+# implemented in Python 2.7 by Stefan Codrescu, see 
+# http://www.policyalmanac.org/games/aStarTutorial.htm
+# http://www.microveggies.com/csci/index.php/9-csci-3202-lecture-notes/28-lecture-7-8-informed-search-a-heuristics
+# for an intro to A* search
+
 import sys
 import math
 
@@ -10,14 +17,12 @@ class World():
 	def get_size(self):
 		return self.size
 	
-	def get_loc(self, loc):
-		# matrix value at loc, 0 is empty, 1 is mountain, 2 is wall
+	def get_loc(self, loc):		# matrix value at loc, 0 is empty, 1 is mountain, 2 is wall
 		return int(self.world[loc[0]][loc[1]])									
 
 	def get_adjacent(self, loc):
 		ret = []
-		# this gives an initial result of the 8 grid points around loc
-		for j in [-1, 1]:
+		for j in [-1, 1]:		# this gives an initial result of the 8 grid points around loc
 			ret.append( [ (loc[0]+j, loc[1]), 10 ] )
 			ret.append( [ (loc[0], loc[1]+j), 10 ] )
 			for i in [-1, 1]:
@@ -25,10 +30,8 @@ class World():
 
 		# then filter them to make sure they are valid, ie no walls or out of bounds
 		return [(x[0], x[1] + self.get_loc(x[0])*10) for x in ret if self.valid_loc(x[0])]
-	
 
-	def valid_loc(self, loc):
-											#check if within bounds of world
+	def valid_loc(self, loc):			#check if within bounds of world
 		if loc[0] < 0 or loc[0] > self.size[0] or loc[1] < 0 or loc[1] > self.size[1]: 
 			return False
 		if self.get_loc(loc) == 2:			#also see if it is a wall
@@ -41,11 +44,10 @@ class Heuristic():
 		self.size = self.world.get_size()
 		self.goal = goal
 
-	def est(self, loc):
-		# compute the heuristic estimate for a given location
+	def est(self, loc):			# compute the heuristic estimate for a given location
 		return abs(self.goal[0] - loc[0]) + abs(self.goal[1] - loc[1])
 
-	def set_goal(self, goal): #helper fn
+	def set_goal(self, goal): 	# helper fn
 		self.goal = goal
 
 class Heuristic_SqrtDist(Heuristic): 			#subclass Heuristic		
@@ -53,8 +55,7 @@ class Heuristic_SqrtDist(Heuristic): 			#subclass Heuristic
 		Heuristic.__init__(self, world, goal)
 
 	def est(self, loc):							#override with new estimate
-		idist = self.goal[0] - loc[0]
-		jdist = self.goal[1] - loc[1]
+		idist = self.goal[0] - loc[0]; jdist = self.goal[1] - loc[1]
 		return math.sqrt( idist * idist + jdist * jdist )
 
 class AStar():
@@ -64,16 +65,12 @@ class AStar():
 
 	def search(self, start, goal):
 		self.heuristic.set_goal(goal)
-		olist = []											# list of open nodes
-		clist = []											# list of closed nodes
-		#the format of olist entries is [selfloc, parentloc, heuristic+upto, uptocost]
-		olist.append([start, start, 0, 0])
+		olist = []; clist = []								# lists containing open and closed nodes
+		olist.append([start, start, 0, 0])					# the format of olist entries is [selfloc, parentloc, heuristic+upto, uptocost]
 		while (len(olist) > 0):
 			node = self.get_min_cost(olist)					# get the minimum heuristic cost open nodes
 			olist.remove(node)								# remove that node from the open list
 			loc, parent, heuristic, cost = node				# explode values to more descriptive names
-	#		print olist
-	#		print "node %s" % node
 			if (loc != goal):								# go until we find the goal
 				clist.append(node)							# add current node to closed since we are evaluating it
 				for adj in self.world.get_adjacent(loc):			# evaluate all valid nodes adjacent to current
@@ -91,6 +88,7 @@ class AStar():
 						except ValueError:
 							olist.append(adjnode)						# otherwise it wasnt in the open list and needs to be added
 			else: 										# executed after goal is found, this finds what the final path was
+				print "Number of locations evaluated: %d" % len(clist)   # <-- ugh, this line is ugly :(
 				path = []								# hold the path as we go back through the closed list to see what path
 				while (loc != start):					# go until we find the start
 					loc, parent, _, _ = node			# explode values to more descriptive names again, dont care about cost and heuristic along way
@@ -98,6 +96,7 @@ class AStar():
 					cindex = [_[0] for _ in clist].index(parent)	# find location of parent in closed
 					node = clist[cindex] 				# set node to parent entry in closed list for next loop
 				return [path[::-1], cost]				# return the path (need to reverse to get start to goal order and total cost)
+		return [None, None] 		# in the case that no path was found
 
 	def get_min_cost(self, olist):
 		# find the minimum heuristic value in open list
@@ -130,28 +129,6 @@ if __name__ == "__main__":
 		h = Heuristic_SqrtDist(w)
 
 	a = AStar(w, h)
-	print a.search( (7, 0), (0, 9) )
-
-"""
-	print "testing world"
-	w = World(which_map)
-	print w.get_size()
-	print w.get_loc( (0,0) )
-	print w.get_adjacent( (0,0) )
-	print w.get_adjacent( (3,3) )
-	print w.get_adjacent( (8, 9) )
-	print w.get_adjacent( (8,1) )
-	print w.valid_loc( (7, 2) )
-	print w.get_loc( (7, 2) )
-	print w.world[7]
-		
-
-	print "testing heuristic"
-	h = Heuristic(w, [0, w.get_size()[1]])
-	print h.est((0, 0))
-	print h.est((7, 0))
-	print h.est((3, 3))
-	print h.est((0, 9))
-"""
-	
+	path, cost = a.search( (7, 0), (0, 9) )
+	print "Path found: %s\nPath cost: %d" % (path, cost)
 
